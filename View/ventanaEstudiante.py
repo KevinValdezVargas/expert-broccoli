60bf29c60dc60f4d3366e511cf1b970028d34bbe
daiko <sm75261486@zegelipae.pe>
2023-07-18 01:07:31 -0500

    mondongo

---
+++ View/ventanaEstudiante.py
@@ -0,0 +1,11 @@
+from PyQt5 import QtWidgets, uic
+from PyQt5.QtWidgets import QWidget
+from Controller.mantenimientoEstudiante import *
+
+aEst = mantenimientoEstudiante()
+
+class ventanaEstudiante(QtWidgets.QMainWindow):
+    def __init__(self, parent = None):
+        super(ventanaEstudiante,self).__init__(parent)
+        uic.loadUi("",self)
+        self.show()
